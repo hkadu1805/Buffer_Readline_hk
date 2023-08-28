a84ca887516acb233f682db10638a8c47a66b557
Harshal Kadu <70891147+hkadu1805@users.noreply.github.com>
2023-08-28 15:15:22 +0530

    Add files via upload

---
+++ readline_ASL.java
@@ -0,0 +1,41 @@
+var defaultS3 = FileFolderService_1("DefaultS3");
+
+var relativeFilePath = @base_location + "/" +
+	@del_sys_name + "/" +
+	@SSF_table_name + "/" +
+	##get_ssf_payload_xmlfile_name(@exec_date, @base_location, @del_sys_name, @SSF_table_name);
+	
+var inStream = defaultS3.getStreamBasedFileReadInfo(relativeFilePath).getStream();
+
+var inStreamReader = new("java.io.InputStreamReader",inStream);
+
+var buffer = new("java.io.BufferedReader",inStreamReader);
+
+var payloadRecordCount = 0;
+
+var search_word = @search_word;
+
+<for> var line = buffer.nextlong() <comma/> line != null <comma/> line = buffer.nextlong()
+	<do>
+		<for> var index = line.indexOf(search_word) <comma/> index &gt; -1 <comma/> index = line.indexOf(search_word)
+			<do>
+				line = line.substring(index + search_word.length());
+				payloadRecordCount++;
+			</do>
+		</for>
+	</do>
+</for>
+
+var matchingIndicator = 'N';
+
+payloadRecordCount = payloadRecordCount + "";
+
+<if> payloadRecordCount.equals(@NR_meta)
+	<then>
+		matchingIndicator = 'Y';
+	</then>
+</if>
+
+out.print(@del_sys_name + '|' + @SSF_table_name + '|' + @NR_meta + '|' + payloadRecordCount + '|' + matchingIndicator);
+
+inStream.close();
